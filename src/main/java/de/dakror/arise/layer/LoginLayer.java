69f7978f848aa37bf6f0c9fd33e945bfb1dad6d3
Dakror <mail@dakror.de>
2014-02-28 22:50:32 +0100

    img

---
+++ src/main/java/de/dakror/arise/layer/LoginLayer.java
@@ -0,0 +1,23 @@
+package de.dakror.arise.layer;
+
+import java.awt.Graphics2D;
+
+import de.dakror.gamesetup.layer.Layer;
+
+/**
+ * @author Dakror
+ */
+public class LoginLayer extends Layer
+{
+	@Override
+	public void draw(Graphics2D g)
+	{}
+	
+	@Override
+	public void update(int tick)
+	{}
+	
+	@Override
+	public void init()
+	{}
+}

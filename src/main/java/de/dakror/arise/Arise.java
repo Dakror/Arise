ed14421f8fc647413f8b03c8ab7c64ad7a0a6546
Dakror <mail@dakror.de>
2014-03-02 19:18:38 +0100

    applet lifecycle

--- src/main/java/de/dakror/arise/Arise.java
+++ src/main/java/de/dakror/arise/Arise.java
@@ -12,10 +12,14 @@ public class Arise extends JApplet
 {
 	private static final long serialVersionUID = 1L;
 	
+	public static boolean running;
+	
 	@Override
 	public void init()
 	{
 		super.init();
+		
+		running = true;
 		
 		new Game();
 		Game.currentApplet.init(this);
@@ -28,12 +32,19 @@ public class Arise extends JApplet
 			@Override
 			public void run()
 			{
-				while (true)
+				while (running)
 				{
 					Game.currentApplet.main();
 				}
 			}
 		}.start();
+	}
+	
+	@Override
+	public void stop()
+	{
+		running = false;
+		Game.currentGame.updater.closeRequested = true;
 	}
 	
 	public static void main(String[] args)

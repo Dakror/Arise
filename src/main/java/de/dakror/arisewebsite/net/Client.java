8c0b19c8c6ef602c59aabdf2015fd60918a55a5d
Dakror <mail@dakror.de>
2014-03-13 20:19:15 +0100

    renamed package -> working on server-client version

---
+++ src/main/java/de/dakror/arisewebsite/net/Client.java
@@ -0,0 +1,15 @@
+package de.dakror.arisewebsite.net;
+
+import java.net.DatagramSocket;
+
+/**
+ * @author Dakror
+ */
+public class Client extends Thread
+{
+	DatagramSocket socket;
+	
+	@Override
+	public void run()
+	{}
+}

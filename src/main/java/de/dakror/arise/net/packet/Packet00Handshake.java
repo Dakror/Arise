8821c45cca4bc31a26c50d9d24d56bd482eebb10
Dakror <mail@dakror.de>
2014-03-13 22:02:02 +0100

    first convertsation

--- src/main/java/de/dakror/arise/net/packet/Packet00Handshake.java
+++ src/main/java/de/dakror/arise/net/packet/Packet00Handshake.java
@@ -5,28 +5,14 @@ package de.dakror.arise.net.packet;
  */
 public class Packet00Handshake extends Packet
 {
-	String username;
-	
-	public Packet00Handshake(String username)
-	{
-		super(0);
-		this.username = username;
-	}
-	
 	public Packet00Handshake()
 	{
-		this("");
-	}
-	
-	public Packet00Handshake(byte[] data)
-	{
 		super(0);
-		username = readData(data);
 	}
 	
 	@Override
 	protected byte[] getPacketData()
 	{
-		return username.getBytes();
+		return "hi".getBytes();
 	}
 }

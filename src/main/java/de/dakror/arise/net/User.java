258385bffba99b479a72748e59f9557caf22e460
Dakror <mail@dakror.de>
2014-03-13 20:26:55 +0100

    server-client model start

---
+++ src/main/java/de/dakror/arise/net/User.java
@@ -0,0 +1,103 @@
+package de.dakror.arise.net;
+
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+/**
+ * @author Dakror
+ */
+public class User
+{
+	private InetAddress ip;
+	private int port;
+	private String username;
+	
+	public User(String username, InetAddress ip, int port)
+	{
+		this.ip = ip;
+		this.port = port;
+		this.username = username;
+	}
+	
+	public User(JSONObject o)
+	{
+		try
+		{
+			if (o.has("i")) ip = InetAddress.getByName(o.getString("i"));
+			if (o.has("p")) port = o.getInt("p");
+			username = o.getString("u");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public InetAddress getIP()
+	{
+		return ip;
+	}
+	
+	public void setIP(InetAddress ip)
+	{
+		this.ip = ip;
+	}
+	
+	public int getPort()
+	{
+		return port;
+	}
+	
+	public void setPort(int port)
+	{
+		this.port = port;
+	}
+	
+	public String getUsername()
+	{
+		return username;
+	}
+	
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+	
+	public String serialize()
+	{
+		JSONObject o = new JSONObject();
+		
+		try
+		{
+			o.put("u", username);
+			o.put("i", ip.getHostAddress());
+			o.put("p", port);
+		}
+		catch (JSONException e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		return o.toString();
+	}
+	
+	public byte[] getBytes()
+	{
+		ByteBuffer bb = ByteBuffer.allocate(username.length() + 5);
+		bb.put((byte) username.length());
+		bb.put(username.getBytes());
+		
+		return bb.array();
+	}
+	
+	@Override
+	public String toString()
+	{
+		return serialize().toString();
+	}
+}
---
+++ src/main/java/de/dakror/arise/net/packet/Packet.java
@@ -0,0 +1,61 @@
+package de.dakror.arise.net.packet;
+
+import java.util.Arrays;
+
+/**
+ * @author Dakror
+ */
+public abstract class Packet
+{
+	public static enum PacketTypes
+	{
+		INVALID,
+		
+		;
+		public int getID()
+		{
+			return ordinal() - 1;
+		}
+	}
+	
+	public byte packetID;
+	
+	public Packet(int packetID)
+	{
+		this.packetID = (byte) packetID;
+	}
+	
+	protected abstract byte[] getPacketData();
+	
+	public byte[] getData()
+	{
+		byte[] strData = getPacketData();
+		
+		byte[] data = new byte[strData.length + 1];
+		data[0] = packetID;
+		
+		System.arraycopy(strData, 0, data, 1, strData.length);
+		
+		return data;
+	}
+	
+	public static String readData(byte[] data)
+	{
+		return new String(Arrays.copyOfRange(data, 1, data.length)).trim();
+	}
+	
+	public PacketTypes getType()
+	{
+		return Packet.lookupPacket(packetID);
+	}
+	
+	public static PacketTypes lookupPacket(int id)
+	{
+		for (PacketTypes pt : PacketTypes.values())
+		{
+			if (pt.getID() == id) return pt;
+		}
+		
+		return PacketTypes.INVALID;
+	}
+}

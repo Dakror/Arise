6a50c6cd06bccf4d3dd49c35e6720141b6c40807
Dakror <mail@dakror.de>
2014-04-24 00:25:57 +0200

    fixed SERVER_CONFIRMED

--- src/main/java/de/dakror/arise/layer/MPLayer.java
+++ src/main/java/de/dakror/arise/layer/MPLayer.java
@@ -5,6 +5,7 @@ import de.dakror.arise.game.Game;
 import de.dakror.arise.net.packet.Packet;
 import de.dakror.arise.net.packet.Packet.PacketTypes;
 import de.dakror.arise.net.packet.Packet02Disconnect;
+import de.dakror.arise.net.packet.Packet02Disconnect.Cause;
 import de.dakror.gamesetup.layer.Alert;
 import de.dakror.gamesetup.layer.Layer;
 import de.dakror.gamesetup.ui.ClickEvent;
@@ -15,23 +16,39 @@ public abstract class MPLayer extends Layer
 	{
 		if (p.getType() == PacketTypes.DISCONNECT)
 		{
-			if (Arise.wrapper)
+			if (((Packet02Disconnect) p).getCause() == Cause.SERVER_CONFIRMED)
 			{
-				Game.currentGame.addLayer(new Alert(((Packet02Disconnect) p).getCause().getDescription(), new ClickEvent()
+				if (Arise.wrapper)
 				{
-					@Override
-					public void trigger()
-					{
-						System.exit(0);
-					}
-				}));
+					System.exit(0);
+				}
+				else
+				{
+					Game.world = null;
+					Game.currentGame.setLayer(new LoginLayer());
+					Game.userID = 0;
+				}
 			}
 			else
 			{
-				Game.world = null;
-				Game.currentGame.setLayer(new LoginLayer());
-				Game.currentGame.addLayer(new Alert(((Packet02Disconnect) p).getCause().getDescription(), null));
-				Game.userID = 0;
+				if (Arise.wrapper)
+				{
+					Game.currentGame.addLayer(new Alert(((Packet02Disconnect) p).getCause().getDescription(), new ClickEvent()
+					{
+						@Override
+						public void trigger()
+						{
+							System.exit(0);
+						}
+					}));
+				}
+				else
+				{
+					Game.world = null;
+					Game.currentGame.setLayer(new LoginLayer());
+					Game.currentGame.addLayer(new Alert(((Packet02Disconnect) p).getCause().getDescription(), null));
+					Game.userID = 0;
+				}
 			}
 		}
 		
